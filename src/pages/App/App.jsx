5386b7771d0de5a43d2ea66b24e55df3a166df90
psteluto <psteluto@bwg.com.br>
2019-10-05 01:22:09 -0300

    front finalizado

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,48 +1,20 @@
 import React from 'react';
 import Axios from 'axios';
-import { Avatar, Button, Card, Col, Collapse, Input, Row, Select, Popconfirm } from 'antd';
+import { Avatar, Button, Card, Col, Collapse, Icon, Input, message, Row, Select, Popconfirm, Tooltip, Typography } from 'antd';
 import appStyle from './App.module.scss';
 import logo from '../../images/logo-mpsp.png'
 
 const { Meta } = Card;
 const { Option } = Select;
 const { Search } = Input;
-const {Panel} = Collapse
-
-const pessoas = [
-    {
-        nome: 'Vinicius Roxo Brandão',
-        cpf: '446.025.698-38',
-        sigla: 'VR',
-    },
-    {
-        nome: 'Henrique dos Santos Miguel',
-        cpf: '365.219.689-90',
-        sigla: 'HS',
-    },
-    {
-        nome: 'Paloma Steluto',
-        cpf: '159.753.458-42',
-        sigla: 'PS',
-    },
-    {
-        nome: 'Isabella Menezes',
-        cpf: '901.789.125-00',
-        sigla: 'IM',
-    },
-    {
-        nome: 'Larissa Carvalho',
-        cpf: '147.258.369-03',
-        sigla: 'LC',
-    },
-];
+const {Panel} = Collapse;
+const {Text} = Typography;
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             current: 0,
-            filterPessoas: pessoas,
             tipoFiltro: "cpf",
             pessoaSelecionada: {},
             rg: "",
@@ -60,30 +32,39 @@ class App extends React.Component {
             numProcesso,
             pis,
         }
+
+        if(!cpfCnpj){
+            message.error('Informar o CPF/CNPJ!!!!')
+        }else{
+            Axios.post("https://127.0.0.1:9090/solicitacoes/registrarSolicitacao", {json}).then(()=> {message.success('Busca cadastrada com sucesso!')}).catch(() => {message.error('Não foi possível cadastrar, por favor tente mais tarde!')})
+        }
+        
     };  
 
-    buscar(value) {
-        Axios.get("localhost:8080/solicitacoes/buscarSolicitacao", {params : value}).then((response)=> {this.setState({pessoa : response.data})}).catch(() => {})
+    buscar(tipoFiltro, value) {
+        Axios.get("127.0.0.1:9090/solicitacoes/buscarSolicitacoes", {params : {tipoFiltro: tipoFiltro, value: value}}).then((response)=> {this.setState({pessoa : response.data})}).catch(() => {})
     }
 
-    gerarRelatorio(pessoaSelecionada) {
+    getRelatorio(pessoaSelecionada) {
         alert("Relatório");
-    }
-
-    filter(lista, value, tipoFiltro) {
-        if (tipoFiltro === "nome") {
-            let nome = value.toLowerCase();
-            this.setState({ filterPessoas: lista.filter((pessoa) => pessoa.nome.toLowerCase().includes(nome)) })
-        } else if (tipoFiltro === "cpf") {
-            let cpf = value.toLowerCase().replace(".", "").replace("-", "");
-            this.setState({ filterPessoas: lista.filter((pessoa) => pessoa.cpf.toLowerCase().replace(".", "").replace("-", "").includes(cpf)) })
-        } else if (tipoFiltro === "rg") {
-            this.setState({ filterPessoas: lista.filter((pessoa) => pessoa.nome.toLowerCase().includes(value.toLowerCase())) })
-        }
     }
 
     confirmar(){
         this.props.history.push('/')
+    }
+
+    getAvatar(nome){
+        nome = "Paloma";
+        return nome.charAt(0);
+    }
+
+    habilitarSelect(pessoa){
+        pessoa = "Concluido";
+        if(pessoa === "Concluido"){
+            this.getRelatorio(this.state.pessoa)
+        }else{
+            message.error('Essa busca ainda não foi concluída, tente novamente mais tarde')
+        }
     }
 
     render() {
@@ -114,12 +95,10 @@ class App extends React.Component {
                                                 <Select defaultValue="cpf"
                                                     onChange={(value) => this.setState({ tipoFiltro: value })}>
                                                     <Option value="cpf">CPF</Option>
-                                                    <Option value="nome">Nome</Option>
                                                     <Option value="rg">RG</Option>
                                                 </Select>
                                             } enterButton="Buscar"                                        
-                                                onSearch={(value) => this.buscar(value)}
-                                                onChange={(e) => this.filter(pessoas, e.target.value, this.state.tipoFiltro)} 
+                                                onSearch={(value) => this.buscar(this.state.tipoFiltro, value)}
                                             />}
                                             </Panel>
                                             
@@ -130,28 +109,45 @@ class App extends React.Component {
                                                 <Input className={appStyle.input} placeholder="Nome" onChange={(e) => {this.setState({nome: e.target.value})}}/>
                                                 <Input className={appStyle.input} placeholder="Número Processo - ARPENSP" onChange={(e) => {this.setState({numProcesso: e.target.value})}}/>
                                                 <Input className={appStyle.input} placeholder="PIS - Trabalhador" onChange={(e) => {this.setState({pis: e.target.value})}}/>
-                                                <Button type="primary" style={{ float: "right", marginTop: "10px", marginBottom: "10px"}} onClick={() => this.cadastrarBusca(this.state.rg, this.state.cpfCnpj, this.state.numProcesso, this.state.pis), Axios.post("localhost:8080/solicitacoes/registrarSolicitacao", this.json).then(()=> {}).catch(() => {})}>Cadastrar busca</Button>
+                                                <Button type="primary" style={{ float: "right", marginTop: "10px", marginBottom: "10px"}} onClick={() => this.cadastrarBusca(this.state.rg, this.state.cpfCnpj, this.state.nome, this.state.numProcesso, this.state.pis)}>Cadastrar busca</Button>
                                             </>}
                                             </Panel>
                                         </Collapse>
                                     </Col>
                                 </Row>
-                                <Row gutter={24}>
-                                    {
-                                        this.state.filterPessoas.map((pessoa) => {
-                                            return (
-                                                <Col xs={24} sm={18} md={14} lg={10} xl={8} xxl={6} className={appStyle.card}>
-                                                    <Card key={pessoa.cpf}
-                                                        actions={[<div
-                                                            onClick={() => this.gerarRelatorio(pessoa)}>Selecionar</div>]}>
-                                                        <Meta className={appStyle.texto} avatar={<Avatar>{pessoa.sigla}</Avatar>} title={pessoa.cpf}
-                                                            description={pessoa.nome} />
-                                                    </Card>
-                                                </Col>
-                                            )
-                                        })
-                                    }
-                                </Row>
+                                {
+                                    !this.state.pessoa &&
+                                    (
+                                    <Col xs={24} sm={18} md={14} lg={10} xl={12} xxl={6} className={appStyle.card}>                            
+                                            <Card actions={[<div
+                                                onClick={() => this.habilitarSelect(this.state.pessoa.status)}>Selecionar</div>]}>
+                                                <Meta
+                                                    avatar={<Avatar>{this.getAvatar(this.state.pessoa.nome)}</Avatar>}
+                                                        title={(
+                                                            <>
+                                                            <Row>
+                                                                <div style={{display: "flex", justifyContent: "space-between"}}>
+                                                                <Text strong>{this.state.pessoa.nome}</Text>
+                                                                {' '}
+                                                                <Tooltip title={this.state.pessoa.status}>
+                                                                    <Icon type="info-circle" />
+                                                                </Tooltip>
+                                                                </div>
+                                                            </Row>
+                                                            </>
+                                                        )}
+                                                            description={this.state.pessoa.cpfCnpj}
+                                                            />
+                                                            <Row>
+                                                            <div style={{ color: '#ccc', marginTop: 10, display: "flex", justifyContent: "flex-end" }}>
+                                                                <Tooltip title="Última atualização">                                    
+                                                                    <span style={{ marginLeft: 10}}>{this.state.pessoa.dataUltimaAtualizacao}</span>
+                                                                </Tooltip>
+                                                            </div>
+                                                            </Row>
+                                                        </Card>                                        
+                                                    </Col>     
+                                    )}                           
                             </div>
                         </div>                
                 </div>
